b3737d3cb7fec60af59fe2297eaecfb11db9d255
John  Quiwa <johnquiwacode@gmail.com>
2018-08-06 23:17:16 -0400

    add spacing

--- client/src/scenes/fileHasher/fileHasherContainer.js
+++ client/src/scenes/fileHasher/fileHasherContainer.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import FileHasher from './fileHasher'
+
 const mapStateToProps = state => ({
   auth: state.auth
 });
